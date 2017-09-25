d24a6332258d8be737ac502b2f0e77e24ff77a30
Kael Shipman <kael.shipman@cfxtrading.com>
2017-09-25 11:27:21 -0500

    Added `parseBody` to `ServerRequestInterface`

--- src/ServerRequestInterface.php
+++ src/ServerRequestInterface.php
@@ -3,6 +3,8 @@ namespace KS\JsonApi;
 
 interface ServerRequestInterface extends \Psr\Http\Message\ServerRequestInterface {
     public function validateProtocol();
+    public function parseBody(FactoryInterface $f);
+
     public function getRequestedResourceType();
     public function getRequestedResourceId();
     public function getRequestedRelationshipName();
