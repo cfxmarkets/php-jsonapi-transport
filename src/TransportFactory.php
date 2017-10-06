954235c7c1401554d60d285a503da8065cbc72e2
Kael Shipman <kael.shipman@cfxtrading.com>
2017-10-06 15:55:45 -0500

    Moved test objects and change factory from trait into class

--- src/TransportFactory.php
+++ src/TransportFactory.php
@@ -1,7 +1,7 @@
 <?php
 namespace KS\JsonApi;
 
-trait TransportFactoryTrait {
+class TransportFactory implements TransportFactoryInterface {
     public function newJsonApiServerRequest($method, $uri, array $headers=[], $body=null, $version='1.1', array $serverParams=[]) { return new ServerRequest($method, $uri, $headers, $body, $version, $serverParams); }
     public function jsonApiServerRequestFromGlobals() { return ServerRequest::fromGlobals(); }
     public function newJsonApiResponse($status=200, array $headers=[], $body=null, $version='1.1', $reason=null) { return new Response($status, $headers, $body, $version, $reason); }
